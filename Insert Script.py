dc0d366edb0912db6a1cfa43900bd5a7ef551c3d
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2023-03-06 09:09:06 +0100

    Added Exercises list with adding script

---
+++ Insert Script.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+with open('bd1.txt', 'r') as f:
+    exercise_names = f.read().splitlines()
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="gympal"
+)
+
+mycursor = mydb.cursor()
+
+for exercise in exercise_names:
+    name, ex_type = exercise.split("|")
+    query = "INSERT INTO exercise (name, type) VALUES (%s, %s)"
+    values = (name.strip(), ex_type.strip())
+    mycursor.execute(query, values)
+    mydb.commit()
+
+mycursor.close()
+mydb.close()
